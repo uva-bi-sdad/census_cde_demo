1333595871541babdcfca804e497ea5813fac96d
Aaron Schroeder <dads2busy@gmail.com>
2022-04-22 15:47:22 +0000

    local data directories for original and working

--- src/00_functions/create_data_dirs.R
+++ src/00_functions/create_data_dirs.R
@@ -1,7 +1,14 @@
-create_data_dirs <- function(data_source_subject_abbrv) {
-  dd_orig <<- file.path("data/original", data_source_subject_abbrv)
-  dd_work <<- file.path("data/working", data_source_subject_abbrv)
+create_data_dirs <- function(data_source_subject_abbrv, local = TRUE) {
+  if (local == TRUE) {
+    dd_orig <<- file.path("data/original_local", data_source_subject_abbrv)
+    dd_work <<- file.path("data/working_local", data_source_subject_abbrv)
+  } else {
+    dd_orig <<- file.path("data/original", data_source_subject_abbrv)
+    dd_work <<- file.path("data/working", data_source_subject_abbrv)
+  }
+  
   dd_docs <<- file.path("docs/01_data", data_source_subject_abbrv)
+  
   dir.create(dd_orig, recursive = TRUE)
   dir.create(dd_work, recursive = TRUE)
   dir.create(dd_docs, recursive = TRUE)

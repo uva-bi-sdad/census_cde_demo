9dc4d8e17d7367a7f79d51471c7008015817c502
Aaron Schroeder <dads2busy@gmail.com>
2022-06-22 15:06:34 -0400

    cms nursing home data for va

---
+++ src/01_data/va_hhs_cms_2014_2022_nursing_home_health_fire_deficiencies/02_prepare_va_unique_providers_2022.R
@@ -0,0 +1,21 @@
+library(data.table)
+library(magrittr)
+library(tidygeocoder)
+
+dt <- fread("data/va_hhs_cms_2014_2022_nursing_home_health_fire_deficiencies/original/va_hhs_cms_2022_nursing_home_providers.csv")
+
+dt_va_unq_dedup_google <- dt %>%
+  geocode(address = location,
+          method="google", 
+          full_results=FALSE)
+
+fwrite(dt_va_unq_dedup_google, "data/va_hhs_cms_2014_2022_nursing_home_health_fire_deficiencies/distribution/va_hhs_cms_2022_nursing_home_providers.csv")
+
+# Update file manifest
+data_file_checksums()
+
+
+# test <- dt_va_unq_dedup_google[, c("federal_provider_number", "provider_name", "lat", "long")]
+# 
+# test_sf <- sf::st_as_sf(test, coords = c("long", "lat"))
+# plot(sf::st_geometry(test_sf))
--- src/01_data/va_hhs_cms_2014_2022_nursing_home_health_fire_deficiencies/03_export.R
+++ src/01_data/va_hhs_cms_2014_2022_nursing_home_health_fire_deficiencies/03_export.R
@@ -16,11 +16,11 @@ dc_dbWriteTable(db_conn = con,
                 table_owner = "census_cde_demo")
 DBI::dbDisconnect(con)
 
-dt <- fread("data/va_hhs_cms_2014_2022_nursing_home_health_fire_deficiencies/distribution/va_hhs_cms_2014_2022_nursing_home_providers.csv")
+dt <- fread("data/va_hhs_cms_2014_2022_nursing_home_health_fire_deficiencies/distribution/va_hhs_cms_2022_nursing_home_providers.csv")
 con <- get_db_conn()
 dc_dbWriteTable(db_conn = con, 
                 schema_name = "census_cde_demo", 
-                table_name = "va_hhs_cms_2014_2022_nursing_home_providers",
+                table_name = "va_hhs_cms_2022_nursing_home_providers",
                 table_data = dt,
                 table_owner = "census_cde_demo")
 DBI::dbDisconnect(con)

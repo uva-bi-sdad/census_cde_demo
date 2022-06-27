a83b34bc5d88d8ac12bd6aed56aae272d7edf7f3
Aaron Schroeder <dads2busy@gmail.com>
2022-06-27 14:47:02 -0400

    2019Q4 cms nurse staffing

--- src/01_data/va_hhs_cms_2020q4_payroll_nurse_staffing/02_prepare.R
+++ src/01_data/va_hhs_cms_2020q4_payroll_nurse_staffing/02_prepare.R
@@ -1,32 +1,23 @@
 # dataset creation code - dataset preparation (transformation, new variables, linkage, etc)
 
 # Import file from original
-va_hhs_cms_2020q4_payroll_nurse_staffing <- sf::st_read("data/va_hhs_cms_2020q4_payroll_nurse_staffing/original/va_hhs_cms_2020q4_payroll_nurse_staffing.json")
+file_paths <- list.files("data/va_hhs_cms_2020q4_payroll_nurse_staffing/original", pattern = "*.json", full.names = T)
 
-va_hhs_cms_2020q4_payroll_nurse_staffing <- 
+if(exists("dt_fin")) rm(dt_fin)
+for (f in file_paths) {
+  dt <- data.table::rbindlist(jsonlite::read_json(f))
+  if(exists("dt_fin")) {
+    dt_fin <- data.table::rbindlist(list(dt_fin, dt))
+  } else {
+    dt_fin <- dt
+  }
+}
 
-# Assign geoid
-va_hhs_cms_2020q4_payroll_nurse_staffing$geoid <- ""
+va_hhs_cms_2020q4_payroll_nurse_staffing <- unique(dt_fin)
 
-# Assign region_type
-va_hhs_cms_2020q4_payroll_nurse_staffing$region_type <- ""
-
-# Assign region_name
-va_hhs_cms_2020q4_payroll_nurse_staffing$region_name <- ""
-
-# Assign year
-va_hhs_cms_2020q4_payroll_nurse_staffing$year <- "2020Q4"
-
-# measure, measure_type, and value need to be included in non-geo datasets
-
-# Select final columns
-final_dataset <- va_hhs_cms_2020q4_payroll_nurse_staffing[, c("geoid", "region_name", "region_type", "year", "geometry", "measure", "measure_unit", "measure_type", "value")]
-
-# Simplify the geography
-final_dataset_simplified <- rmapshaper::ms_simplify(final_dataset)
 
 # Export final dataset
-sf::st_write(final_dataset_simplified, "data/va_hhs_cms_2020q4_payroll_nurse_staffing/distribution/va_hhs_cms_2020q4_payroll_nurse_staffing.geojson")
+data.table::fwrite(va_hhs_cms_2020q4_payroll_nurse_staffing, "data/va_hhs_cms_2020q4_payroll_nurse_staffing/distribution/va_hhs_cms_2020q4_payroll_nurse_staffing.csv")
 
 # Update file manifest
 data_file_checksums()
